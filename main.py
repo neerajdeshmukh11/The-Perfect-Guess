6975d519af0cd0d5f1a5560569cdf16d92c46d89
Neeraj Deshmukh <neerajdeshmukh547@gmail.com>
2024-03-14 13:04:49 +0530

    The Perfect Guess

---
+++ main.py
@@ -0,0 +1,18 @@
+import random
+randNumber=random.randint(1,100)
+guesses=0
+# userguess=None
+while(userguess!=randNumber):
+    userguess=int(input("Enter yor Guess: "))
+    guesses+=1
+    if(userguess==randNumber):
+        print("Bravo!! You guessed it right")   
+    else:
+        if(randNumber>userguess):
+            print("oops!! You guessed it wrong, enter a larger guess")
+        else:    
+            print("oops!! You guessed it wrong, enter a smaller guess")
+print(f"You took total {guesses} chances to guess the number")
+
+with open("hiscore.txt", "r") as f:
+    hiscore=f.read()
